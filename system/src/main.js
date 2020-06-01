7ff4e03daef64c1941ea09eed4b54c29294c629f
hjyclover <44895303+hjyclover@users.noreply.github.com>
2020-06-01 13:54:55 +0800

    判断ie版本是否低于9,低于9给出提示

--- system/src/main.js
+++ system/src/main.js
@@ -1,3 +1,6 @@
+
+
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -6,6 +9,8 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI);
+
+
 
 Vue.config.productionTip = false
 
